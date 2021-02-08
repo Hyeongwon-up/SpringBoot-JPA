9182eb74512ecb58f4a0710c5cf29476c8dc9b9d
Hyeongwon-up <leehwdatabase@gmail.com>
2021-02-08 14:12:55 +0900

    update

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.service;
 
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,7 +20,16 @@ public class ItemService {
     public void saveItem(Item item) {
         itemRepository.save(item);
     }
-    
+
+
+    @Transactional
+    public void updateItem(Long itmeId, String name, int price, int stockQuantity){
+        Item findItem = itemRepository.findOne(itmeId);
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
+
+    }
     public List<Item> findItems() {
         return itemRepository.findAll();
     }
---
+++ src/test/java/jpabook/jpashop/service/ItemUpdateTest.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.service;
+
+
+import jpabook.jpashop.domain.item.Book;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+
+@SpringBootTest
+public class ItemUpdateTest {
+
+    @Autowired
+    EntityManager em;
+    @Test
+    public void updateTest() throws Exception{
+        //given
+        Book book = em.find(Book.class, 1L);
+        //when
+
+        book.setName("asdasdsa");
+
+        //변경감지
+        
+        //then
+    }
+}
